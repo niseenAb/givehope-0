05344fc7d3263907d68cf3dbc6c2bea780961a7e
niseenAb <nisrin0460@gmail.com>
2026-01-06 20:03:46 +0200

    Add files via upload

---
+++ backend/utils/multer.js
@@ -0,0 +1,26 @@
+const multer = require('multer');
+
+const fileValidation = {
+    image: ['image/png', 'image/jpeg', 'image/webp'],
+    pdf: ['application/pdf'],
+};
+
+function fileUpload(customValidation = []) {
+    const storage = multer.diskStorage({});
+
+    function fileFilter(req, file, cb) {
+        if (customValidation.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb("invalid format", false);
+        }
+    }
+
+    const upload = multer({ fileFilter, storage });
+    return upload;
+}
+
+module.exports = {
+    fileUpload,
+    fileValidation
+};
